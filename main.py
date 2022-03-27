194ce99085e0f7fa6eae23fe05b6778fd004c370
Udit Ghosh <46648384+uditGhosh10@users.noreply.github.com>
2022-03-27 15:09:29 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/') # this is the home page route
+def hello_world(): # this is the home page function that generates the page code
+    return "Hello world!"
+    
+@app.route('/webhook', methods=['POST'])
+def webhook():
+  req = request.get_json(silent=True, force=True)
+  fulfillmentText = ''
+  sum = 0
+  query_result = req.get('queryResult')
+  if query_result.get('action') == 'sub.numbers':
+    num1 = int(query_result.get('parameters').get('number'))
+    num2 = int(query_result.get('parameters').get('number1'))
+    sum = str(num1 + num2)
+    print('here num1 = {0}'.format(num1))
+    print('here num2 = {0}'.format(num2))
+    fulfillmentText = 'The sum of the two numbers is '+sum
+  elif query_result.get('action') == 'multiply.numbers':
+    num1 = int(query_result.get('parameters').get('number'))
+    num2 = int(query_result.get('parameters').get('number1'))
+    product = str(num1 * num2)
+    print('here num1 = {0}'.format(num1))
+    print('here num2 = {0}'.format(num2))
+    fulfillmentText = 'The product of the two numbers is '+product
+  return {
+        "fulfillmentText": fulfillmentText,
+        "source": "webhookdata"
+    }
+    
+   
+if __name__ == '__main__':
+  app.run() 
