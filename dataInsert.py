0dd9dc2cee9523e6721b18df3a064fc899f0270b
Vatsal Rathod <vatsalrathod16@gmail.com>
2022-04-24 20:07:34 -0400

    major changes done

--- dataInsert.py
+++ dataInsert.py
@@ -1,5 +1,6 @@
 import pymongo
 import json
+import certifi
 
 '''
 firebaseConfig = {"apiKey": "AIzaSyDxHLOgqjQeYUnniDeMI4gsNZWS-Y3XQ1s",
@@ -9,25 +10,26 @@ firebaseConfig = {"apiKey": "AIzaSyDxHLOgqjQeYUnniDeMI4gsNZWS-Y3XQ1s",
     "messagingSenderId": "81643804817",
     "appId": "1:81643804817:web:300757ad8a36c423b90730",
     "measurementId": "G-7KCX675SD7",
-    "databaseURL":"https://fisfirebasedb-default-rtdb.firebaseio.com/"}'''
+    "databaseURL":"https://fisfirebasedb-default-rtdb.firebaseio.com/"}
 
 
-#firebase = pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(firebaseConfig)
 
-#db = firebase.database()
+db = firebase.database()
 
-#data = {"name" : "John", "age":20}
-#db.push(data)
+data = {"name" : "John", "age":20}
+db.push(data)'''
 
-client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
-db = client['FIS_Chatbot']
-collection = db['locations']
+client = pymongo.MongoClient('mongodb+srv://FISChatbot:ZMdaSTA6jkBqKmBo@clusterfis.cd6hr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',tlsCAFile=certifi.where())
+db = client.get_database('FIS_Chatbot')
+collection = db['loc']
 requesting = []
 with open(r"my_data.json") as f:
     # for jsonObj in f:
         myDict = json.load(f)
-        print(myDict)
+        #print(myDict)
         #collection.insert_one(myDict)
+print(myDict)
 collection.insert_many(myDict)
 
 client.close()
--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from math import cos, asin, sqrt
 import heapq
 import pymongo
 import pandas as pd
+from smtplib import SMTP_SSL as SMTP
+from email.message import EmailMessage
 
 
-'''
-firebaseConfig = {"apiKey": "AIzaSyDxHLOgqjQeYUnniDeMI4gsNZWS-Y3XQ1s",
+
+
+'''firebaseConfig = {"apiKey": "AIzaSyDxHLOgqjQeYUnniDeMI4gsNZWS-Y3XQ1s",
     "authDomain": "fisfirebasedb.firebaseapp.com",
     "projectId": "fisfirebasedb",
     "storageBucket": "fisfirebasedb.appspot.com",
@@ -18,7 +21,8 @@ firebaseConfig = {"apiKey": "AIzaSyDxHLOgqjQeYUnniDeMI4gsNZWS-Y3XQ1s",
 
 # data = {"name" : "John", "age":20}
 # db.push(data)
-client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
+
+client = pymongo.MongoClient('mongodb+srv://FISChatbot:ZMdaSTA6jkBqKmBo@clusterfis.cd6hr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 db = client['FIS_Chatbot']
 
 
@@ -96,31 +100,168 @@ def webhook():
     # print()
     m=[str(df.iloc[i[1]].location_name) for i in heapq.nsmallest(5, r)]
     fulfillmentText="\n ".join(m)
-    print(g)
   elif query_result.get('action') == 'bookAppointment':
     Location = str(query_result.get('parameters').get('any'))
     col1 = db['locations']
     df = "-1"
-    #df = col1.find_one({},{'location_name':Location})
     df = pd.DataFrame(list(col1.find()))
     try:
       v=df[df['location_name']==Location]
       print(v)
-      #fulfillmentText = 'Appointment Booked'
     except Exception:
       fulfillmentText = 'please enter valid pantry name'
-
     if(len(v)==0):
       fulfillmentText = 'please enter valid pantry name'
     else:
-      fulfillmentText = 'Appointment Booked'
-      
+      fulfillmentText = 'Confirm Location'
+  elif query_result.get('action') == 'locationconfirm':
+    Location = str(query_result.get('parameters').get('any'))
+    fulfillmentText = 'Confirm Email Address'
+  elif query_result.get('action') == 'CheckStatus':
+    #print('hello')
+    input = str(query_result.get('parameters').get('any'))
+    location = input[0]
+    dt = input[1]
+    col = db['Appointment']
+    df = pd.DataFrame(list(col.find()))
+    donor_count=len(df[(df['location_name']==location) & (df['date']==dt) & (df['usertype']=='Donor')])
+    recieve_count=len(df[(df['location_name']==location) & (df['date']==dt) & (df['usertype']=='Donor')])
+    fulfillmentText = 'Donor count ='+str(donor_count)+'\n Reciever count='+str(recieve_count)
+  elif query_result.get('action') == 'CheckStatusPantry':
+    #print('hello')
+    dt = str(query_result.get('parameters').get('any'))
+    #location = input[0]
+    #dt = input[1]
+    col = db['Appointment']
+    df = pd.DataFrame(list(col.find()))
+    fulfillmentText = df[(df['date']==dt) & (df['usertype']=='Donor')]
+    fulfillmentText = dict(fulfillmentText.groupby('location_name').size())
+    for i in fulfillmentText:
+      temp=i+' : '+str(fulfillmentText[i])+'\n'
+    fulfillmentText = temp
+    print(fulfillmentText)
+    #fulfillmentText = 'hello'
+  elif query_result.get('action') == 'CheckStatusPantry':
+    try:
+      s = SMTP('smtp.gmail.com', 465)
+      s.login("realmonster7777@gmail.com", "youmonster")
+      msg = EmailMessage()
+      msg.set_content('This is my message')
+      msg['Subject'] = 'Subject'
+      msg['From'] = "realmonster7777@gmail.com"
+      msg['To'] = "vatsalrathod16@gmail.com"
+      s.send_message(msg)
+      s.quit()
+    except Exception:
+      print(Exception)
+  elif query_result.get('action') == 'BookAppointment':
+    
+    location_name = str(query_result.get('parameters').get('any'))
+    email = str(query_result.get('parameters').get('email'))
+    dt = str(query_result.get('parameters').get('date'))
+    col1 = db['locations']
+    df = "-1"
+    col = db['Appointment']
+    #df = col1.find_one({},{'location_name':Location})
+    df = pd.DataFrame(list(col1.find()))
+    Location = 'I.S. 117'
+    try:
+        v=df[df['location_name']==Location]
+        print(v)
+        #fulfillmentText = 'Appointment Booked'
+    except Exception:
+        fulfillmentText = 'please enter valid pantry name'
+        
+    if(len(v)==0):
+        fulfillmentText = 'please enter valid pantry name'
+    else:
+        #fulfillmentText = 'Appointment Booked'
+        addDict = {'name' : 'Vatsal',
+        'email' : email,
+        'phone' : 7043879131,
+        'live_appointment' : 'Yes',
+        'Appointment_Status' : 'Yes',
+        'Appointment_time'  : '7:00',
+        'usertype' : 'Donor'}
+        print(col.insert_one(addDict))
 
+    fulfillmentText = 'Appointment booked! kindly check your email'
+    try:
+      # print('Hello')
+      s = SMTP('smtp.gmail.com', 465)
+      s.login("realmonster7777@gmail.com", "youmonster")
+      msg = EmailMessage()
+      message_content = 'Pantry Location : '+location_name+'\n'+'date :'+dt
+      msg.set_content(message_content)
+      msg['Subject'] = 'Pantry Appointment Booked!'
+      msg['From'] = "realmonster7777@gmail.com"
+      msg['To'] = email
+      s.send_message(msg)
+      s.quit()
+    except Exception:
+      print(Exception)  
+    
+
+#s.quit()
   return {
         "fulfillmentText": fulfillmentText,
         "source": "webhookdata"
     }
 
-   
+'''app = Flask(__name__)
+@app.route('/webhook', methods=['POST', 'GET'])
+def webhook():
+  req = request.get_json(silent=True, force=True)
+  fulfillmentText = ''
+  sum = 0
+  query_result = req.get('queryResult')
+  print(query_result)
+  if query_result.get('action') == 'sub.numbers':
+    num1 = int(query_result.get('parameters').get('number'))
+    num2 = int(query_result.get('parameters').get('number1'))
+    sum = str(num1 + num2)
+    print('here num1 = {0}'.format(num1))
+    print('here num2 = {0}'.format(num2))
+    fulfillmentText = 'The sum of the two numbers is '+sum
+  elif query_result.get('action') == 'multiply.numbers':
+    num1 = int(query_result.get('parameters').get('number'))
+    num2 = int(query_result.get('parameters').get('number1'))
+    product = str(num1 * num2)
+    print('here num1 = {0}'.format(num1))
+    print('here num2 = {0}'.format(num2))
+    fulfillmentText = 'The product of the two numbers is '+product
+
+  elif query_result.get('action')=='book.custom':
+    def distance(lat1, lon1, lat2, lon2, df):
+        # df=pd.read_json('data.json')
+        p = 0.017453292519943295
+        hav = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p)*cos(lat2*p) * (1-cos((lon2-lon1)*p)) / 2
+        return 12742 * asin(sqrt(hav))
+
+    def closest(v, data, df):
+        r=[]
+        for i in range(len(v)):
+            r.append([distance(v[i][0],v[i][1], data[0],data[1], df), i])
+        heapq.heapify(r)
+        return heapq.nsmallest(5, r)
+
+    print('1111111')
+    num1 = (query_result.get('parameters').get('any'))
+    print(num1)
+    df=pd.read_json('data.json')
+    print(df)
+    tempDataList = [[i,j] for i,j in zip(df.location_latitude, df.location_longitude)]
+    print(tempDataList)
+    res=closest(tempDataList,[39.7622290, -86.1519750], df)
+    y=[str(df.iloc[i[1]].location_name) for i in res]
+
+    fulfillmentText = 'The product of the two numbers is '+ "".join(y)
+    print(fulfillmentText)
+
+  return {
+        "fulfillmentText": fulfillmentText,
+        "source": "webhookdata"
+    }'''
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5002)
